04db2823396305813dc72e6e68f1c6ccc2cc3018
Shri10 <pethe.shriraj@gmail.com>
2023-04-20 12:53:53 +0530

    Added: to usecase 4

--- arithmatic.sh
+++ arithmatic.sh
@@ -4,6 +4,6 @@ read -p "Enter the value of b: " b
 read -p "Enter the value of c: " c
 
 
-#UC3
-result2=$(awk "BEGIN {print $a * $b + $c}")
-echo "Result of a * b + c : " $result2
+#UC4
+result3=$(awk "BEGIN {print $c + $a / $b}")
+echo "Result of c + a / b : " $result3
