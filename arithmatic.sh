3e996d929500db8e4dbd49de30308eb7b0279399
Shri10 <pethe.shriraj@gmail.com>
2023-04-20 12:55:37 +0530

    Added: to usecase 6

--- arithmatic.sh
+++ arithmatic.sh
@@ -4,6 +4,22 @@ read -p "Enter the value of b: " b
 read -p "Enter the value of c: " c
 
 
-#UC5
+# UC2
+result1=$(awk "BEGIN {print $a + $b * $c}")
+
+# UC3
+result2=$(awk "BEGIN {print $a * $b + $c}")
+
+# UC4
+result3=$(awk "BEGIN {print $c + $a / $b}")
+
+# UC5
 result4=$(awk "BEGIN {print $a % $b + $c}")
-echo "Result of a % b + c : " $result4
+
+#UC6
+declare -A results
+results=( ["a + b * c"]=$result1 ["a * b + c"]=$result2 ["c + a / b"]=$result3 ["a % b + c"]=$result4 )
+
+for key in "${!results[@]}"; do
+  echo "Equation: $key - Result: ${results[$key]}"
+done
