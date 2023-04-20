295a5a198719256ec25b685c671c9adb19867092
Shri10 <pethe.shriraj@gmail.com>
2023-04-20 13:42:23 +0530

    Merging uc4

--- arithmatic.sh
+++ arithmatic.sh
@@ -3,7 +3,6 @@ read -p "Enter the value of a: " a
 read -p "Enter the value of b: " b
 read -p "Enter the value of c: " c
 
-
 #UC2
 result1=$(awk "BEGIN {print $a + $b * $c}")
 echo "Result of a + b * c : " $result1
